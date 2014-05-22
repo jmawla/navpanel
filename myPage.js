a5eabf154f103de30939fbf59c4c8a4aac8d4452
Jamal <jmawla@yahoo.com>
2014-05-22 15:47:40 -0600

    more changes

--- myPage.js
+++ myPage.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+	var tmp = "hello";
+	console.log(tmp);
 	$('.navButton').mousenter(function() {
 		var $children = $(this).children('.subButton');
 		$children.slideToggle('fast');
